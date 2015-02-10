72dc3964991c22d51cd3f912d91ad2e172b46e34
lq <lq@le1.ca>
2015-02-10 13:59:28 -0700

    pull in sousselk's xbee drivers

---
+++ boards/ek-tm4c1294xl/include/xbee-config.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2014 INRIA
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @defgroup    driver_xbee XBee driver
+ * @ingroup     drivers
+ * @brief       XBee autonomous 802.15.4 modem driver
+ * @{
+ *
+ * @file
+ * @brief       Configuration values for the XBee 802.15.4 modem driver
+ *
+ * @author      Kévin Roussel <kevin.roussel@inria.fr>
+ */
+
+#ifndef __XBEE_CONFIG_H
+#define __XBEE_CONFIG_H
+
+#include "periph/uart.h"
+
+#define XBEE_UART_LINK   UART_7
+
+#endif /* __XBEE_CONFIG_H */
