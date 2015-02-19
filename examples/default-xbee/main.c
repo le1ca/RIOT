4dc958adbb6e573cbe2312f5439293f8ee9560ff
lq <lq@le1.ca>
2015-02-19 11:20:42 -0700

    add demo file for xbee

---
+++ examples/default-xbee/main.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2008, 2009, 2010  Kaspar Schleiser <kaspar@schleiser.de>
+ * Copyright (C) 2013 INRIA
+ * Copyright (C) 2013 Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Default application that shows a lot of functionality of RIOT
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ * @author      Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include "thread.h"
+#include "posix_io.h"
+#include "shell.h"
+#include "shell_commands.h"
+#include "board_uart0.h"
+
+//#include "ieee802154_frame.h"
+#include "xbee.h"
+
+static int shell_readc(void){
+    char c = 0;
+    (void) posix_read(uart0_handler_pid, &c, 1);
+    return c;
+}
+
+static void shell_putchar(int c){
+    (void) putchar(c);
+}
+
+void xbee_rx(void *buf, unsigned int len, int8_t rssi, uint8_t lqi, bool crc_ok){
+	char* cb = (char*) buf;
+	printf("[xbee] got packet, rsi %d, crc %d, length is %d\n", rssi, crc_ok? 1 : 0, len);
+	for(int i = 0; i < len; i++){
+		printf("%02x ", cb[i]);
+	}
+	printf("\n\n");
+}
+
+int main(void)
+{
+    shell_t shell;
+    (void) posix_open(uart0_handler_pid, 0);
+
+	printf("[xbee] doing init\n");
+	xbee_radio_driver.init();
+	printf("[xbee] setting rx cb\n");
+	xbee_radio_driver.set_receive_callback(xbee_rx);
+
+    (void) puts("Welcome to RIOT!");
+
+    shell_init(&shell, NULL, UART0_BUFSIZE, shell_readc, shell_putchar);
+
+    shell_run(&shell);
+    return 0;
+}
